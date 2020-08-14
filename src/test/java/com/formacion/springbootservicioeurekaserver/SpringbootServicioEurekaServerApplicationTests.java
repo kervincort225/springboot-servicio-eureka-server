aca3b3be25629cdabc0557f572427fdeacda3dee
kervi <kervincort225@gmail.com>
2020-08-14 08:20:00 -0400

    eureka server, works for register the servicies

---
+++ src/test/java/com/formacion/springbootservicioeurekaserver/SpringbootServicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacion.springbootservicioeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
